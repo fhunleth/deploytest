7f9c4b30fa3138223ea2cf080ecb30e773293665
Frank Hunleth <fhunleth@troodon-software.com>
2015-12-09 09:53:46 -0500

    More stuff

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -2,8 +2,10 @@
 
 # Check if we're building under Travis-ci
 if [ "$TRAVIS" = "true" ]; then
-    echo "TAG=$TRAVIS_TAG"
-    echo "BRANCH=$TRAVIS_BRANCH"
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+        echo "Not supposed to be deploying pull requests!!"
+        exit 1
+    fi
 
     if [ -n "$TRAVIS_TAG" ]; then
         BRANCH_OR_TAG=$TRAVIS_TAG
@@ -11,6 +13,8 @@ if [ "$TRAVIS" = "true" ]; then
         BRANCH_OR_TAG=$TRAVIS_BRANCH
     fi
 else
+    # Use git to figure out the branch or tag. This doesn't
+    # work on Travis due to how it clones the builds.
     BRANCH_OR_TAG=$(git describe --exact-match 2>/dev/null || git rev-parse --abbrev-ref HEAD)
 fi
 
@@ -18,4 +22,4 @@ SYSTEM_ARCHIVE_NAME=deploytest-$BRANCH_OR_TAG
 
 rm -fr artifacts
 mkdir artifacts
-cp README.md artifacts/$SYSTEM_ARCHIVE_NAME.txt
+cp build-product.txt artifacts/$SYSTEM_ARCHIVE_NAME.txt
