03b2ede6b2e4daca613cb9501eb7975df20ab9b4
Frank Hunleth <fhunleth@troodon-software.com>
2015-12-09 09:41:03 -0500

    Improvements

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Check if we're building under Travis-ci
+if [ $TRAVIS = true ]; then
+    if [ $TRAVIS_TAG ]; then
+        BRANCH_OR_TAG=$TRAVIS_TAG
+    else
+        BRANCH_OR_TAG=$TRAVIS_BRANCH
+    fi
+fi
+
 BRANCH_OR_TAG=$(git describe --exact-match 2>/dev/null || git rev-parse --abbrev-ref HEAD)
 SYSTEM_ARCHIVE_NAME=deploytest-$BRANCH_OR_TAG
 
